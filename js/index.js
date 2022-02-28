b0914f6c83466f5a9d8d767fcc5dc77a2fdb9306
el <zahra.sharafian1797@gmail.com>
2022-02-28 22:18:27 +0330

    add new styles change color of elements and font add main sections of game

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+
+let squares = ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty']
+let player1Score = 0;
+let player2Score = 0;
+
+replay.onclick = function myfunction() {
+    for (let i = 0; i < 9; i++) {
+        squares[i] = 'empty';
+    }
+    player1Score = 0;
+    player2Score = 0;
+    document.getElementsByTagName
+    console.log("nn");
+}
