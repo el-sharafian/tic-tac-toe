fc989e1f92b8c2ae4939e520028f3fced9d22bf5
el <zahra.sharafian1797@gmail.com>
2022-04-11 20:43:30 +0430

    edit style.css and index.js add restart button edit functions and variables

--- js/index.js
+++ js/index.js
@@ -1,97 +1,99 @@
-let squares = [0, 0, 0, 0, 0, 0, 0, 0, 0]       // 0 is empty, 1 is x, 2 is o
+let squares = new Array(9).fill("");
 let playerXScore = 0;
 let playerOScore = 0;
 let XO = 'x';
 let winner = false;
 let numberOfX = 5;
-let numberOfO = 5;
+let numberOfO = 4;
 
-replay.onclick = function replayGame() {
-    for (let i = 0; i < 9; i++) {
-        squares[i] = 0;
-    }
+for (let i = 0; i < 9; i++) {
+    document.getElementsByClassName("game_sec")[0].getElementsByTagName("div")[i].onclick = () => setSquare(i);
+}
+document.getElementById("replay").onclick = () => replayGame();
+document.getElementById("restart").onclick = () => restartGame();
+
+function replayGame() {
+    squares.fill("");
     winner = false;
     XO = 'x';
     numberOfX = 5;
     numberOfO = 4;
     displayElements();
 }
+function restartGame() {
+    playerXScore = 0;
+    playerOScore = 0;
+    document.getElementById("scoresX").textContent = playerXScore;
+    document.getElementById("scoresO").textContent = playerOScore;
+    replayGame();
+}
 function displayElements() {
-    document.getElementById("winnerX").style.display = "none";
-    document.getElementById("winnerO").style.display = "none";
+    document.getElementById("winner").style.display = "none";
     for (let j = 0; j < 9; j++) {
         document.getElementsByClassName("game_sec")[0].getElementsByTagName('img')[j].style.display = "none";
     }
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 5; i++) {
         document.getElementsByClassName("piece")[0].getElementsByTagName('img')[i].style.display = "inline-flex";
-        document.getElementsByClassName("piece")[1].getElementsByTagName('img')[i].style.display = "inline-flex";
+        if (i < 4) {
+            document.getElementsByClassName("piece")[1].getElementsByTagName('img')[i].style.display = "inline-flex";
+        }
     }
-
 }
-function setSquare(event, squareNumber) {
-    changeImg(squareNumber);
-}
-function changeImg(i) {
-    if (squares[i] == 0 && winner == false) {
+function setSquare(i) {
+    if (squares[i] == "" && winner == false) {
         if (XO == 'x') {
-            document.getElementById("square" + i).getElementsByTagName('img')[0].src = "./img/icon-x.png";
-            squares[i] = 1;     // 1 is x
+            document.getElementsByClassName("game_sec")[0].getElementsByTagName("div")[i].getElementsByTagName('img')[0].src = "./img/icon-x.png";
+            squares[i] = 'x';
             XO = 'o';
             document.getElementsByClassName("piece")[0].getElementsByTagName('img')[numberOfX - 1].style.display = "none";
             numberOfX--;
         }
         else {
-            document.getElementById("square" + i).getElementsByTagName('img')[0].src = "./img/icon-o.png";
-            squares[i] = 2;     // 2 is o
+            document.getElementsByClassName("game_sec")[0].getElementsByTagName("div")[i].getElementsByTagName('img')[0].src = "./img/icon-o.png";
+            squares[i] = 'o';
             XO = 'x';
             document.getElementsByClassName("piece")[1].getElementsByTagName('img')[numberOfO - 1].style.display = "none";
             numberOfO--;
         }
-        document.getElementById("square" + i).getElementsByTagName('img')[0].style.display = "block";
+        document.getElementsByClassName("game_sec")[0].getElementsByTagName("div")[i].getElementsByTagName('img')[0].style.display = "block";
         checkBoard();
     }
 }
 function checkBoard() {
     for (let i = 0; i < 7; i += 3) {
-        if (squares[i] == squares[i + 1] && squares[i] == squares[i + 2] && squares[i] != 0) {
+        if (squares[i] != "" && squares[i] == squares[i + 1] && squares[i] == squares[i + 2]) {
             chooseWinner(i);
             break;
         }
     }
     for (let i = 0; i < 3; i++) {
-        if (squares[i] == squares[i + 3] && squares[i + 3] == squares[i + 6] && squares[i] != 0) {
+        if (squares[i] != "" && squares[i] == squares[i + 3] && squares[i + 3] == squares[i + 6]) {
             chooseWinner(i);
             break;
         }
     }
-    if (squares[0] == squares[4] && squares[4] == squares[8] && squares[0] != 0) {
+    if (squares[0] != "" && squares[0] == squares[4] && squares[4] == squares[8]) {
         chooseWinner(0);
     }
-    else if (squares[2] == squares[6] && squares[6] == squares[4] && squares[2] != 0) {
+    else if (squares[2] != "" && squares[2] == squares[6] && squares[6] == squares[4]) {
         chooseWinner(2);
     }
+    else if (numberOfX == 0 && winner == false) {
+        document.getElementById("winner").textContent = "DRAW";
+        document.getElementById("winner").style.display = "block";
+    }
 }
-
 function chooseWinner(i) {
-    if (squares[i] == 1) {
-        document.getElementById("winnerX").style.display = "block";
+    if (squares[i] == 'x') {
+        document.getElementById("winner").textContent = "winner is X";
         playerXScore++;
-        document.getElementById("scoresX").innerHTML = playerXScore;
-        winner = true;
+        document.getElementById("scoresX").textContent = playerXScore;
     }
     else {
-        document.getElementById("winnerO").style.display = "block";
+        document.getElementById("winner").textContent = "winner is O";
         playerOScore++;
-        document.getElementById("scoresO").innerHTML = playerOScore;
-        winner = true
+        document.getElementById("scoresO").textContent = playerOScore;
     }
+    document.getElementById("winner").style.display = "block";
+    winner = true;
 }
-document.getElementById("square0").onclick = (event) => setSquare(event, 0);
-document.getElementById("square1").onclick = (event) => setSquare(event, 1);
-document.getElementById("square2").onclick = (event) => setSquare(event, 2);
-document.getElementById("square3").onclick = (event) => setSquare(event, 3);
-document.getElementById("square4").onclick = (event) => setSquare(event, 4);
-document.getElementById("square5").onclick = (event) => setSquare(event, 5);
-document.getElementById("square6").onclick = (event) => setSquare(event, 6);
-document.getElementById("square7").onclick = (event) => setSquare(event, 7);
-document.getElementById("square8").onclick = (event) => setSquare(event, 8);
